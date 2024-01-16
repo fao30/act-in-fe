817b0313a73532633186f794af99ba4aa4a2cc3f
Ryan Pratama <ru.ryanpratama@gmail.com>
2024-01-16 18:02:26 +0700

    adjust

--- src/app/survey/page.tsx
+++ src/app/survey/page.tsx
@@ -1,36 +1,33 @@
 "use client";
 
-import { cn } from "@/libs/functions";
-import logo from "#/assets/logo.svg";
-import { type StaticImport } from "next/dist/shared/lib/get-img-props";
+import { motion } from "framer-motion";
 import dynamic from "next/dynamic";
-import Image from "next/image";
-import { Fragment, useEffect, useState } from "react";
+import { Fragment } from "react";
 
 const SurveyContainer = dynamic(() => import("./components/SurveyContainer"), { ssr: false });
 
 export default function SurveyPage() {
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const loadingTimer = setTimeout(() => setLoading(false), 1 * 1000);
-    return () => {
-      clearTimeout(loadingTimer);
-    };
-  }, []);
-
   return (
     <Fragment>
-      <article
-        className={cn("fixed bg-white top-0 w-full flex min-h-[100dvh] items-center justify-center z-50", { "hidden ": !loading })}
-      >
-        {loading ? (
-          <section className="flex gap-8 items-center animate-pulse">
-            <Image src={logo as StaticImport} alt="ACT IN" className="w-24 aspect-auto" />
-            <h1 className="text-shaft font-bold">ACT IN</h1>
-          </section>
-        ) : null}
-      </article>
+      <motion.section
+        initial={{ x: "100%", width: "100%" }}
+        animate={{ x: "0%", width: "0%" }}
+        transition={{ duration: 1, ease: "easeIn" }}
+        className="fixed top-0 bottom-0 right-full w-screen h-screen z-50 bg-white"
+      />
+      <motion.section
+        initial={{ x: "100%", width: "100%" }}
+        animate={{ x: "0%", width: "0%" }}
+        transition={{ delay: 0.2, duration: 1, ease: "easeIn" }}
+        className="fixed top-0 bottom-0 right-full w-screen h-screen z-40 bg-barley"
+      />
+      <motion.section
+        initial={{ x: "100%", width: "100%" }}
+        animate={{ x: "0%", width: "0%" }}
+        transition={{ delay: 0.4, duration: 1, ease: "easeIn" }}
+        className="fixed top-0 bottom-0 right-full w-screen h-screen z-30 bg-tango"
+      />
+
       <SurveyContainer />
     </Fragment>
   );
